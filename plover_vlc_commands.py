e6ca48df473db1da613ab01c15fe66b5bacfd6a7
Benoit Pierre <benoit.pierre@gmail.com>
2017-02-05 17:54:16 +0100

    initial import

---
+++ plover_vlc_commands.py
@@ -0,0 +1,63 @@
+
+import os
+import json
+
+import requests
+
+from plover.oslayer.config import CONFIG_DIR
+
+
+_VLC_CONFIG = {
+    'host': 'localhost',
+    'port': '8080',
+    'user': '',
+    'pass': 'password',
+}
+
+_vlc_config = dict(_VLC_CONFIG)
+_vlc_config_path = os.path.join(CONFIG_DIR, 'vlc.json')
+if os.path.exists(_vlc_config_path):
+    with open(_vlc_config_path) as fp:
+        _vlc_config.update(json.load(fp))
+
+_vlc_url = 'http://%s:%s/requests/status.json' % (
+    _vlc_config['host'], _vlc_config['port']
+)
+_vlc_auth = (_vlc_config['user'], _vlc_config['pass'])
+
+
+def _vlc_request(path=''):
+    r = requests.get(_vlc_url + path, auth=_vlc_auth)
+    r.raise_for_status()
+    return r
+
+
+def pause(engine, cmdline):
+    _vlc_request('?command=pl_forcepause')
+    return ''
+
+def resume(engine, cmdline):
+    _vlc_request('?command=pl_forceresume')
+    return ''
+
+def seek(engine, cmdline):
+    _vlc_request('?command=seek&val='+cmdline)
+    return ''
+
+def stop(engine, cmdline):
+    _vlc_request('?command=pl_stop')
+    return ''
+
+def timestamp(engine, cmdline):
+    r = _vlc_request()
+    seconds = r.json()['time']
+    hours = seconds // 3600
+    seconds -= hours * 3600
+    minutes = seconds // 60
+    seconds -= minutes * 60
+    timestamp = '%02u:%02u:%02u' % (hours, minutes, seconds)
+    return timestamp
+
+def toggle_pause(engine, cmdline):
+    _vlc_request('?command=pl_pause')
+    return ''
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name = 'plover_vlc_commands',
+    version = '0.5.0',
+    description = 'VLC commands for Plover',
+    author = 'Benoit Pierre',
+    author_email = 'benoit.pierre@gmail.com',
+    license =  'GNU General Public License v2 or later (GPLv2+)',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: End Users/Desktop'
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires = [
+        'plover>=4.0.0.dev0',
+        'requests',
+    ],
+    py_modules = [
+        'plover_vlc_commands',
+    ],
+    entry_points = '''
+
+    [plover.command]
+    VLC_pause        = plover_vlc_commands:pause
+    VLC_resume       = plover_vlc_commands:resume
+    VLC_seek         = plover_vlc_commands:seek
+    VLC_stop         = plover_vlc_commands:stop
+    VLC_timestamp    = plover_vlc_commands:timestamp
+    VLC_toggle_pause = plover_vlc_commands:toggle_pause
+
+    ''',
+    zip_safe = True,
+)
