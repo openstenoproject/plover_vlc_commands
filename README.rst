fb27c4805221a4fdd5215eae86b4f202239531ed
Benoit Pierre <benoit.pierre@gmail.com>
2017-03-31 05:35:52 +0200

    setup: move metadata out of setup.py, add README

---
+++ README.rst
@@ -0,0 +1,39 @@
+Plover VLC commands
+===================
+
+Add support for controlling VLC from Plover.
+
+Setup
+-----
+
+-  create ``vlc.json`` in the same directory as your configuration, set
+   those settings that differ from defaults, e.g.:
+
+   .. code:: json
+
+       {
+         "host": "localhost",
+         "port": "8080",
+         "user": "",
+         "pass": "password"
+       }
+
+-  start VLC with the HTTP interface enabled, e.g.:
+   ``vlc --extraintf http --http-password password ...``
+
+Available commands
+------------------
+
+-  ``VLC_pause``, ``VLC_resume``, ``VLC_stop``, ``VLC_toggle_pause``:
+   self explanatory
+-  ``VLC_seek``: seek the video, for example ``{PLOVER:VLC_seek:-10s}``
+   to seek backward 10 seconds, ``{PLOVER:VLC_seek:+1m}`` to seek
+   forward 1 minute
+
+Timestamp support
+-----------------
+
+A custom meta to get the current video timestamp is also provided:
+``VLC_timestamp``. For example using
+``{[inaudible ^}{:VLC_timestamp}{^]}`` would translate to something
+like: ``[inaudible 00:08:01]``.
--- setup.py
+++ setup.py
@@ -2,45 +2,4 @@
 
 from setuptools import setup
 
-
-setup(
-    name = 'plover_vlc_commands',
-    version = '0.5.0',
-    description = 'VLC commands for Plover',
-    author = 'Benoit Pierre',
-    author_email = 'benoit.pierre@gmail.com',
-    license =  'GNU General Public License v2 or later (GPLv2+)',
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Plugins',
-        'Intended Audience :: End Users/Desktop'
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    install_requires = [
-        'plover>=4.0.0.dev0',
-        'requests',
-    ],
-    py_modules = [
-        'plover_vlc_commands',
-    ],
-    entry_points = '''
-
-    [plover.command]
-    VLC_pause        = plover_vlc_commands:pause
-    VLC_resume       = plover_vlc_commands:resume
-    VLC_seek         = plover_vlc_commands:seek
-    VLC_stop         = plover_vlc_commands:stop
-    VLC_toggle_pause = plover_vlc_commands:toggle_pause
-
-    [plover.meta]
-    VLC_timestamp    = plover_vlc_commands:timestamp
-
-    ''',
-    zip_safe = True,
-)
+setup()
